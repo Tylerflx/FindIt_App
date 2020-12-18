89ce83c2cbdc85ec940694bb59a9256437b00c1c
Tyler <ruafino17@gmail.com>
2020-12-18 05:33:09 -0500

    Finished

--- app/src/main/java/com/example/finalproject_tylernguyen/Adapter.java
+++ app/src/main/java/com/example/finalproject_tylernguyen/Adapter.java
@@ -16,18 +16,30 @@ import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     LayoutInflater inflater;
-     List<Job> jobs; //contain all the jobs
+    List<Job> jobs; //contain all the jobs
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String currentUserId = user.getUid();
+    private Context context;
 
-     public Adapter(Context ctx, List<Job> jobs){
-         //to receive the jobs from adapter
-         this.inflater = LayoutInflater.from(ctx);
-         this.jobs = jobs;
-     }
+    public Adapter(Context ctx, List<Job> jobs){
+        //to receive the jobs from adapter
+        this.inflater = LayoutInflater.from(ctx);
+        this.jobs = jobs;
+        this.context=ctx;
+    }
     @NonNull
     //assign layout
 
@@ -49,7 +61,41 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         holder.job_location.setText(jobs.get(position).getJob_location());
         Picasso.get().load(jobs.get(position).getLogo_url()).into(holder.company_logo);
 
+        //if heart button on click
+        holder.fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //access fav list in database
+                holder.fav_list = database.getReference("FavoriteList").child(currentUserId).child(jobs.get(position).getJob_id());
+                //set job
+                final String name = jobs.get(position).getJob_title();
+                //fill the heart button if on click
+                holder.fav_list.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //if data on change then set the fav button to full red
+                        holder.fav.setImageResource(R.drawable.ic_favourite);
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        //if unchange then keep the same
+                        holder.fav.setImageResource(R.drawable.ic_favorite_border);
 
+                    }
+                });
+                //add to database
+                holder.fav_list.setValue(name);
+                Toast.makeText(v.getContext(), "Added to Favourite List", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.job_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent it = new Intent(context, JobInfo.class);
+                it.putExtra("id", jobs.get(position).getJob_id());
+                context.startActivity(it);
+            }
+        });
     }
     //number of items that will be view
     @Override
@@ -67,7 +113,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            context = itemView.getContext();
+            job = new Job();
             //assign items
             job_title = itemView.findViewById(R.id.jobTitle);
             job_type = itemView.findViewById(R.id.positionTitle);
--- app/src/main/java/com/example/finalproject_tylernguyen/Favourite.java
+++ app/src/main/java/com/example/finalproject_tylernguyen/Favourite.java
@@ -2,19 +2,84 @@ package com.example.finalproject_tylernguyen;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Favourite extends AppCompatActivity {
+    //RecyclerView recyclerView;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference;
+    FirebaseUser user;
+    List<Job> jobs;
+    Adapter adapter;
+    private RecyclerView fav_list;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+        fav_list = findViewById(R.id.fav_view);
+        ArrayList<String> list = new ArrayList<>();
+        //fav_list = findViewById(R.id.result_view);
+        jobs = new ArrayList<>();
+        adapter = new Adapter(getApplicationContext(), jobs);
+        fav_list.setAdapter(adapter);
+
+        //firebase user
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            //if user exist then display
+            String currentUserId = user.getUid();
+            reference = database.getReference().child("FavouriteList").child(currentUserId);
+            //recall from database
+            // Read from the database
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    list.clear();
+                    for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        list.add(snapshot.getValue().toString());
+                    }
+                    //adapter.notifyDataSetChanged();
+                    //Toast.makeText(getApplicationContext(), "Added to Favourite List", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    System.out.println("The read failed: " + error.getCode());
+                }
+            });
+
+        }
+        //Toast.makeText(getApplicationContext(), "User uid: " + currentUserId, Toast.LENGTH_SHORT).show();
+
+
+
         //init
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         //Set Home Select
